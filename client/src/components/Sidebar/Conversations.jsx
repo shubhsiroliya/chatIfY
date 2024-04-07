07d37e88d4f34ab400cb9bf7230b50f43ec715ed
shubhsiroliya <siroliya.shubh@gmail.com>
2024-04-07 13:01:07 +0530

    User Interface Part Completed

---
+++ client/src/components/Sidebar/Conversations.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Converstation from './Converstation'
+
+const Conversations = () => {
+  return (
+    <div className='py-2 flex flex-col overflow-auto'>
+        <Converstation/>
+        <Converstation/>
+        <Converstation/>
+        <Converstation/>
+        <Converstation/>
+        <Converstation/>
+    </div>
+  )
+}
+
+export default Conversations
